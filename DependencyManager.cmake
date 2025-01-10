83fea1ba3e258df3387a8a8f4523838767c47aca
Robert Adam <dev@robert-adam.de>
2025-01-10 17:54:17 +0100

    Add stub for DependencyManager.cmake file

---
+++ DependencyManager.cmake
@@ -0,0 +1,5 @@
+cmake_minimum_required(VERSION 3.19...3.31 FATAL_ERROR)
+
+include("${CMAKE_CURRENT_LIST_DIR}/cmake/log.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/cmake/verify_parsed_arguments.cmake")
+
